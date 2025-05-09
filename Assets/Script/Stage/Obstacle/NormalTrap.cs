7b88a33b64293dc007be4f28f42929ff30b1a6f9
Chaemin Sohn <cmsohn20@naver.com>
2025-05-09 11:56:17 +0900

    fix : 함정 데미지 처리 로직 수정
     - 함정 데미지 처리 로직 수정

--- Assets/Script/Stage/Obstacle/NormalTrap.cs
+++ Assets/Script/Stage/Obstacle/NormalTrap.cs
@@ -7,14 +7,37 @@ public class NormalTrap : MonoBehaviour   //플레이어 접촉 시 피해를 입히는 함정
     [Header("Trap Settings(Base)")]
     [SerializeField] protected float damageAmount = 10f;        // 피해량
     [SerializeField] protected LayerMask playerLayer;           // 피해 대상 레이어(플레이어)
+    //protected PlayerResource player  데미지 처리를 위한 플레이어 체력 관리 객체
 
     protected virtual void OnTriggerEnter2D(Collider2D other)
     {
         if (((1 << other.gameObject.layer) & playerLayer) != 0)
         {
-            //데미지 처리
+            //player = other.GetComponent<PlayerResource>();
+            TryDealDamage();
         }
     }
 
+    protected virtual void OnTriggerStay2D(Collider2D other)
+    {
+        if (((1 << other.gameObject.layer) & playerLayer) != 0)
+        {
+            TryDealDamage();
+        }
+    }
+
+    protected virtual void OnTriggerExit2D(Collider2D other)
+    {
+        /*
+         * if(player != null && other.gameObject = player.gameObject){
+         *      player = null;
+         */
+    }
+
+    protected void TryDealDamage()
+    {
+        //if(player == null) return;
+        //데미지 처리
+    }
 }
 
--- Assets/Script/Stage/Obstacle/PeriodicTrap.cs
+++ Assets/Script/Stage/Obstacle/PeriodicTrap.cs
@@ -9,7 +9,7 @@ public class PeriodicTrap : NormalTrap   //활성화/비활성화를 반복하는 함정
     [SerializeField] private float inactiveDuration = 2.0f;   // 함정이 비활성화되어 있는 시간
 
     private bool isActive = false;      //현재 함정 활성화 상태
-    private float timer = 0f;          //주기 변경을 위한 타이머
+    private float activeTimer = 0f;          //주기 변경을 위한 타이머
     private Collider2D trapCollider;
     private Animator animator;
     private readonly int IsActive = Animator.StringToHash("IsActive");
@@ -17,13 +17,13 @@ public class PeriodicTrap : NormalTrap   //활성화/비활성화를 반복하는 함정
     private void Start()
     {
         Init();
-        SetTrapState(true);   //비활성화 상태로 시작   
+        SetTrapState(true);   //활성화 상태로 시작   
     }
 
     private void Update()
     {
-        timer -= Time.deltaTime;
-        if(timer <= 0f)
+        activeTimer -= Time.deltaTime;
+        if(activeTimer <= 0f)
         {
             //함정 상태 전환
             SetTrapState(!isActive);
@@ -42,36 +42,33 @@ public class PeriodicTrap : NormalTrap   //활성화/비활성화를 반복하는 함정
         animator.SetBool(IsActive, isActive);
         if (isActive)
         {
-            timer = activeDuration;
-            CheckAndDamagePlayerInside();
-            
+            activeTimer = activeDuration;   
         }
         else
         {
-            timer = inactiveDuration;
+            activeTimer = inactiveDuration;
         }   
     }
 
-    void CheckAndDamagePlayerInside()
-    {   
-        //함정 활성화 시, 플레이어가 이미 밟고 있으면 데미지 처리
-        //OverlapBox 를 활용하여 플레이어 레이어 감지
-        Bounds trapBounds = trapCollider.bounds;
-
-        Collider2D playerCollider = Physics2D.OverlapBox(
-            trapBounds.center,  trapBounds.size, 0, playerLayer);
-
-        if (playerCollider != null)
+    protected override void OnTriggerEnter2D(Collider2D other)
+    {
+        if (isActive &&  ((1 << other.gameObject.layer) & playerLayer) != 0)
         {
-            //데미지 처리
+            TryDealDamage();
         }
     }
-    protected override void OnTriggerEnter2D(Collider2D other)
+
+    protected override void OnTriggerStay2D(Collider2D other)
     {
         if (isActive && ((1 << other.gameObject.layer) & playerLayer) != 0)
         {
-            //데미지 처리
+            TryDealDamage();
         }
+    }
+
+    protected override void OnTriggerExit2D(Collider2D other)
+    {
+        base.OnTriggerExit2D(other);
     }
 }
 
