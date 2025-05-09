b5b74541b2fead764bdb572e7c068ae81eb9b835
nwoorang <89909664+nwoorang@users.noreply.github.com>
2025-05-09 14:02:41 +0900

    add:오브젝트풀링,이벤트 추가

--- Assets/Script/Skill/ProjectileController.cs
+++ Assets/Script/Skill/ProjectileController.cs
@@ -1,19 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ProjectileController : MonoBehaviour {
+
+public class ProjectileController : MonoBehaviour
+{
     private ProjectileData data;//비어있는 투사체의 데이터
     private Vector2 direction;//비어있는 공격방향
     private Rigidbody2D rb;//프리팹한 투사체의 리짓바디
+    public event Action<GameObject> OnClick;
 
-    public void Init(Vector2 dir, ProjectileData _data) {
+    public void Init(Vector2 dir, ProjectileData _data)
+    {
         direction = dir.normalized;
         data = _data;
         rb = GetComponent<Rigidbody2D>();
-
         GetComponent<SpriteRenderer>().color = data.color;
-        Destroy(gameObject, data.duration);
+        Invoke("WrappingInvoke", data.duration);
     }
 
     private void FixedUpdate()//물리처리
@@ -21,4 +25,16 @@ public class ProjectileController : MonoBehaviour {
         rb.velocity = direction * data.speed;
         transform.Rotate(Vector3.forward, data.rotateSpeed * Time.fixedDeltaTime);
     }
+
+    void OnTriggerEnter2D(Collider2D collision)//충돌했을 시
+    {
+        CancelInvoke("WrappingInvoke");
+        OnClick?.Invoke(gameObject);//skillShooter의 ReturnToPool() 메서드를 호출
+    }
+
+    void WrappingInvoke()//Invoke(name,time) 지연 호출을 쓰려면 함수 이름으로 넣어줘야 하므로 래핑
+    {
+        OnClick?.Invoke(gameObject);
+    }
+
 }
