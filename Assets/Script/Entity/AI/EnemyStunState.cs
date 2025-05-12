4c21e5e030391fedef58ff93f23304b19c574676
송승윤 <119779039+tmddbs134679@users.noreply.github.com>
2025-05-12 09:42:17 +0900

    feat : Stun Animation 적용

---
+++ Assets/Script/Entity/AI/EnemyStunState.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStunState : EnemyBaseState
+{
+    private readonly int StunHas = Animator.StringToHash("Stun");
+    private const float AnimatorDampTime = 0.1f;
+    private const float CrossFadeDuration = 0.1f;
+
+    private float duration = 1f;
+
+    public EnemyStunState(EnemyStateMachine stateMachine) : base(stateMachine) { }
+
+    public override void Enter()
+    {
+        duration = 1f;
+        stateMachine.Animator.CrossFadeInFixedTime(StunHas, CrossFadeDuration);
+    }
+
+    public override void Tick(float deltaTime)
+    {
+        duration -= deltaTime;
+
+        if (duration <= 0f)
+        {
+            stateMachine.SwitchState(stateMachine.States[EENEMYSTATE.IDLE]);
+        }
+    }
+    public override void Exit()
+    {
+        
+    }
+
+}
