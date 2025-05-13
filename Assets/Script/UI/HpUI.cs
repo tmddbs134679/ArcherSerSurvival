2f737a79fb4502efb301d3b69c028315f31bfc22
송승윤 <119779039+tmddbs134679@users.noreply.github.com>
2025-05-13 14:19:55 +0900

    feat : BossHP 추가

--- Assets/Script/UI/HpUI.cs
+++ Assets/Script/UI/HpUI.cs
@@ -1,36 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HpUI : MonoBehaviour
 {
     [SerializeField] private EnemyStat enemyStat;
-    [SerializeField] Slider slider;
+    [SerializeField] private Slider slider;
+
+
 
     private void Awake()
     {
-     
+        if (enemyStat == null)
+            enemyStat = GetComponentInParent<EnemyStat>();
+
+        
     }
 
     private void OnEnable()
     {
         if (enemyStat != null)
-        {
             enemyStat.OnTakeDamage += UpdateHpBar;
-        }
     }
 
     private void OnDisable()
     {
         if (enemyStat != null)
-        {
             enemyStat.OnTakeDamage -= UpdateHpBar;
-        }
     }
 
     private void UpdateHpBar()
     {
+     
         if (enemyStat != null)
         {
             slider.value = enemyStat.CurrentHp / enemyStat.MaxHp;
