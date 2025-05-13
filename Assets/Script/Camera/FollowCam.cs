1e2862d19d3540940b63ed68379fe66751c0495e
mingyeon <74883641+sichelh@users.noreply.github.com>
2025-05-13 15:48:04 +0900

    fix : player prefab에 카메라 제거, 카메라 프리팹 추가 및 이동 구현

--- Assets/Script/Camera/FollowCam.cs
+++ Assets/Script/Camera/FollowCam.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class FollowCam : MonoBehaviour
 {
     private Transform target;
-    private Vector3 offset;
+    private Vector3 fixedXZ;
+    [SerializeField] private float moveSpeed = 2f;
+    [SerializeField] private float moveMinY = 1f;
+    [SerializeField] private float moveMaxY = 6.15f;
 
     private void Start()
     {
         target = FindObjectOfType<PlayerController>().transform;
-        offset = transform.position - target.position; 
+        fixedXZ = new Vector3(transform.position.x, 0, transform.position.z);
     }
 
     private void LateUpdate()
     {
-        transform.position = target.position + offset;
+        float targetY = Mathf.Clamp(target.position.y, moveMinY, moveMaxY);
+        float lerpY = Mathf.Lerp(transform.position.y, targetY, Time.deltaTime * moveSpeed);
+
+        transform.position = new Vector3(fixedXZ.x, lerpY, fixedXZ.z);
     }
 }
