c328564663bcf256c7957747bd196c368718f2e4
Chaemin Sohn <cmsohn20@naver.com>
2025-05-13 23:05:54 +0900

    feat : 플레이어 공격 SFX

--- Assets/Script/Controller/Player/PlayerSFXControl.cs
+++ Assets/Script/Controller/Player/PlayerSFXControl.cs
@@ -8,6 +8,10 @@ public class PlayerSFXControl : MonoBehaviour
     [SerializeField] private AudioClip hit;
     [SerializeField] private AudioClip die;
 
+    [SerializeField] private Dictionary<string, AudioClip> projectileSFX = new Dictionary<string, AudioClip>();
+    
+    [SerializeField] private AudioClip[] weaponSounds;
+
     private AudioSource audioSource;
 
     private void Start()
@@ -16,6 +20,14 @@ public class PlayerSFXControl : MonoBehaviour
         if(audioSource == null)
         {
             Debug.LogWarning("Player Audio Source Not Found");
+        }
+
+        foreach(AudioClip clip in weaponSounds)
+        {
+            string name = clip.name;
+            Debug.Log(clip);
+            projectileSFX.Add(name.Substring(0, clip.name.Length - "_Sound".Length), clip);
+            Debug.Log(name.Substring(0, clip.name.Length - "_Sound".Length));
         }
     }
 
@@ -31,8 +43,15 @@ public class PlayerSFXControl : MonoBehaviour
     {
         audioSource.PlayOneShot(die);
     }
-    public void OnAttackAxe()
+    public void OnAttack(string name)
     {
-
+        Debug.Log(name);
+        AudioClip clip = projectileSFX[name];
+        Debug.Log(clip);
+        if (clip == null)
+        {
+            return;
+        }
+        audioSource.PlayOneShot(clip);
     }
 }
