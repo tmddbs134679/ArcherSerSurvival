8a212b51daa018be5ab67cf8845266252cbe73b3
송승윤 <119779039+tmddbs134679@users.noreply.github.com>
2025-05-14 22:15:12 +0900

    fix : 버그수정

--- Assets/Script/Skill/ChangedSkillData.cs
+++ Assets/Script/Skill/ChangedSkillData.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class ChangedSkillData : MonoBehaviour
+public class ChangedSkillData 
 {
     /*
     public float speed;
