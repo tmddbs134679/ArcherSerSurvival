02096de54ffad63f288d2b03eb4644206f1b8b66
송승윤 <119779039+tmddbs134679@users.noreply.github.com>
2025-05-15 10:10:03 +0900

    fix : 풀링 최적화

--- Assets/Script/Skill/DamagePool.cs
+++ Assets/Script/Skill/DamagePool.cs
@@ -8,29 +8,52 @@ public class DamagePool : Singleton<DamagePool>
     public DamageText prefab;
     public Canvas worldCanvas;
     private Queue<DamageText> dmgPool = new Queue<DamageText>();
+    private int lastExpandCount = 10;
+
+    private void Start()
+    {
+       
+        for (int i = 0; i < lastExpandCount; i++)
+        {
+            var obj = Instantiate(prefab, worldCanvas.transform);
+            obj.gameObject.SetActive(false);
+            dmgPool.Enqueue(obj);
+        }
+    }
+
+
     public DamageText Get()
     {
-        if (dmgPool.Count > 0)
+        if (dmgPool.Count == 0)
         {
-            var t = dmgPool.Dequeue();
-            t.gameObject.SetActive(true);
-            return t;
+         
+            int expandCount = lastExpandCount * 2;
+            for (int i = 0; i < expandCount; i++)
+            {
+                var obj = Instantiate(prefab, worldCanvas.transform);
+                obj.gameObject.SetActive(false);
+                dmgPool.Enqueue(obj);
+            }
+
+            lastExpandCount = expandCount;
         }
 
-        var instance = Instantiate(prefab, worldCanvas.transform);
-        return instance;
-    }
-    public void Release(DamageText text)
-    {
-        text.gameObject.SetActive(false);
-        dmgPool.Enqueue(text);
+        var text = dmgPool.Dequeue();
+        text.gameObject.SetActive(true);
+        return text;
     }
 
-    public void ShowDamageText(int damage, Vector3 position)
+    public void Release(DamageText dmgText)
+    {
+        dmgText.gameObject.SetActive(false);
+        dmgPool.Enqueue(dmgText);
+    }
+
+    public void ShowDamageText(int dmg, Vector3 pos)
     {
         DamageText damageText = Get();
         damageText.gameObject.SetActive(true);
-        damageText.Show(damage, position); 
+        damageText.Show(dmg, pos); 
     }
 
 }
